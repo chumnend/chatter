2a27fca0ecb1b4afc13ca6cd168b22e4ae87518f
Nicholas Chumney <nicholas.chumney@outlook.com>
2020-10-24 14:02:05 -0400

    added linter

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 'use strict';
 
-const express = require("express");
-const morgan = require("morgan");
-const config = require("./config");
-const socket = require("./socket");
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const config = require('./config');
+const socket = require('./socket');
 
 // initilaize the application
 const app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-if( config.env !== 'test') {
-    app.use(morgan("common"));
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+if (config.env !== 'test') {
+  app.use(morgan('common'));
 }
 
 // setup application routes
-app.get("/", (req, res) => {
-    res.sendFile('index.html', {root: __dirname + "/public"});
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname + '/public' });
 });
 
 // add sockets
@@ -25,7 +27,7 @@ const server = socket(app);
 
 // listen to server
 server.listen(config.port, () => {
-    console.log("Server started on port", config.port);
+  console.log('Server started on port', config.port);
 });
 
 module.exports = server;
--- src/socket.js
+++ src/socket.js
@@ -1,119 +1,112 @@
-function socket(app) 
-{
-    const server = require('http').createServer(app);
-    const io = require('socket.io')(server);
+function socket(app) {
+  const server = require('http').createServer(app);
+  const io = require('socket.io')(server);
 
-    var chatDetails = {
-        users: [],
-        typing: []
+  var chatDetails = {
+    users: [],
+    typing: [],
+  };
+
+  function lookupUser(socket) {
+    let idx = chatDetails.users.findIndex((user) => user.id == socket.id);
+
+    return {
+      idx,
+      user: chatDetails.users[idx],
     };
+  }
 
-    function lookupUser(socket) {
-        let idx = chatDetails.users.findIndex( 
-                user => user.id == socket.id 
-            );
+  io.on('connection', (socket) => {
+    socket.on('register', (prevName, fn) => {
+      let assignedName;
+      if (prevName && !chatDetails.users.some((u) => u.name == assignedName)) {
+        assignedName = prevName;
+      } else {
+        assignedName = 'guest-' + socket.id.substr(0, 5);
+      }
 
-        return { 
-            idx,
-            user: chatDetails.users[idx]
-        };
-    }
+      // record information about the new client
+      let user = { id: socket.id, name: assignedName };
+      chatDetails.users.push(user);
 
-    io.on('connection', socket => {
-        socket.on("register", (prevName, fn) => {
-            let assignedName; 
-            if (prevName && !chatDetails.users.some(u => u.name == assignedName) ) {
-                assignedName = prevName;
-            }
-            else {
-                assignedName = "guest-" + socket.id.substr(0, 5);
-            }
+      // returns to new client, whos in the chat and assigned name
+      fn({ users: chatDetails.users, name: assignedName });
 
-            // record information about the new client 
-            let user = { id: socket.id, name: assignedName };
-            chatDetails.users.push(user);
-
-            // returns to new client, whos in the chat and assigned name
-            fn({ users: chatDetails.users, name: assignedName }); 
-
-            // inform all other clients of the new client 
-            socket.broadcast.emit("alert", `${assignedName} has joined the room`);
-            socket.broadcast.emit("update", chatDetails);
-        });
-
-        socket.on("message", (res) => {
-            let { sender, message } = res;
-            
-            // broadcast new message to other clients
-            socket.broadcast.emit("message", { sender, message });
-        });
-
-        socket.on("private message", (res) => {
-            let { sender, reciever, message } = res;
-            
-            // lookup socket id
-            let targetUser = chatDetails.users.find(
-                user => user.name  === reciever
-            );
-            
-            if(targetUser) {
-                // broadcast new message intended reciever
-                io.to(targetUser.id).emit("private message", { sender, message });
-            }
-            else {
-                socket.emit("alert", `${reciever} is not a valid user`);
-            }
-        });
-
-        socket.on("name change", (newName, fn) => {
-            // check if name already taken
-            if( chatDetails.users.some(u => u.name == newName) ) {
-                fn(false);
-            } else {
-                let { user } = lookupUser(socket);
-                
-                let oldName = user.name; 
-                user.name = newName;
-                
-                io.emit("alert", `${oldName} changed name to ${newName}`);
-                io.emit("update", chatDetails);
-                fn(true);
-            }
-        });
-        
-        socket.on("start typing", () => {
-            let { user } = lookupUser(socket);
-            
-            chatDetails.typing.push(user.name);
-            
-            socket.broadcast.emit("update", chatDetails);
-        });
-        
-        socket.on("stop typing", () => {
-            let { user } = lookupUser(socket);
-            
-            chatDetails.typing = chatDetails.typing.filter( t => {
-                t !== user.name;
-            });
-            
-            socket.broadcast.emit("update", chatDetails);
-        });
-
-        socket.on('disconnect', () => {
-            let { user } = lookupUser(socket);
-
-            // update clients currently in the chat
-            chatDetails.users = chatDetails.users.filter( 
-                user => user.id !== socket.id
-            );
-
-            // broadcasts to to clients about disconnecting client
-            io.emit("alert", `${user.name} has left the room`);
-            socket.broadcast.emit("update", chatDetails);
-        });
+      // inform all other clients of the new client
+      socket.broadcast.emit('alert', `${assignedName} has joined the room`);
+      socket.broadcast.emit('update', chatDetails);
     });
 
-    return server;
+    socket.on('message', (res) => {
+      let { sender, message } = res;
+
+      // broadcast new message to other clients
+      socket.broadcast.emit('message', { sender, message });
+    });
+
+    socket.on('private message', (res) => {
+      let { sender, reciever, message } = res;
+
+      // lookup socket id
+      let targetUser = chatDetails.users.find((user) => user.name === reciever);
+
+      if (targetUser) {
+        // broadcast new message intended reciever
+        io.to(targetUser.id).emit('private message', { sender, message });
+      } else {
+        socket.emit('alert', `${reciever} is not a valid user`);
+      }
+    });
+
+    socket.on('name change', (newName, fn) => {
+      // check if name already taken
+      if (chatDetails.users.some((u) => u.name == newName)) {
+        fn(false);
+      } else {
+        let { user } = lookupUser(socket);
+
+        let oldName = user.name;
+        user.name = newName;
+
+        io.emit('alert', `${oldName} changed name to ${newName}`);
+        io.emit('update', chatDetails);
+        fn(true);
+      }
+    });
+
+    socket.on('start typing', () => {
+      let { user } = lookupUser(socket);
+
+      chatDetails.typing.push(user.name);
+
+      socket.broadcast.emit('update', chatDetails);
+    });
+
+    socket.on('stop typing', () => {
+      let { user } = lookupUser(socket);
+
+      chatDetails.typing = chatDetails.typing.filter((t) => {
+        t !== user.name;
+      });
+
+      socket.broadcast.emit('update', chatDetails);
+    });
+
+    socket.on('disconnect', () => {
+      let { user } = lookupUser(socket);
+
+      // update clients currently in the chat
+      chatDetails.users = chatDetails.users.filter(
+        (user) => user.id !== socket.id,
+      );
+
+      // broadcasts to to clients about disconnecting client
+      io.emit('alert', `${user.name} has left the room`);
+      socket.broadcast.emit('update', chatDetails);
+    });
+  });
+
+  return server;
 }
 
 module.exports = socket;
